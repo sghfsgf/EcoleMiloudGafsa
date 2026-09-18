85f5086f501d1d4f2760417b6510049f1fb05139
sgh <esghaier.ainformation@gmail.com>
2026-09-18 08:17:02 +0100

    Update Firebase configuration with new API key and app ID

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,40 @@
 /* =========================================================
    FIREBASE - ECOLE MILOUD GAFSA
+   Authentication + Firestore
+   ========================================================= */
+
+
+/* =========================================================
+   FIREBASE APP
    ========================================================= */
 
 import { initializeApp }
     from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
 
+
+/* =========================================================
+   FIREBASE AUTHENTICATION
+   ========================================================= */
+
 import { getAuth }
     from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
+
+
+/* =========================================================
+   FIRESTORE
+   ========================================================= */
 
 import { getFirestore }
     from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
 
 
+/* =========================================================
+   CONFIGURATION FIREBASE
+   ========================================================= */
+
 const firebaseConfig = {
 
-    apiKey: "VOTRE_API_KEY",
+    apiKey: "AIzaSyDejW6NPVy9lfP2CIblz_dWc7xsgDyizwM",
 
     authDomain: "ecolemiloudgafsa.firebaseapp.com",
 
@@ -24,12 +44,26 @@ const firebaseConfig = {
 
     messagingSenderId: "1088965354679",
 
-    appId: "VOTRE_APP_ID"
+    appId: "1:1088965354679:web:6eb1fddfcf34dffb781932"
 };
 
 
+/* =========================================================
+   INITIALISATION DE FIREBASE
+   ========================================================= */
+
 const app = initializeApp(firebaseConfig);
 
+
+/* =========================================================
+   EXPORT AUTHENTIFICATION
+   ========================================================= */
+
 export const auth = getAuth(app);
+
+
+/* =========================================================
+   EXPORT FIRESTORE
+   ========================================================= */
 
 export const db = getFirestore(app);
